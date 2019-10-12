0c70f85a03a0d82a25e4a3714016c0eeaaf30193
Gregory Grubbs <gregory@dynapse.com>
2019-10-11 22:21:42 -0600

    stow future config

--- emacs/gjg-functions.el
+++ emacs/gjg-functions.el
@@ -196,4 +196,49 @@ With prefix, restrict to files currently being visited"
                           (shell-quote-argument "{}") " "
                           (shell-quote-argument ";"))))
 
+;; TODO: integrate lsp-mode maybe someday
+
+;; ** LSP-Mode
+;; #+begin_src emacs-lisp :tangle no
+;; (use-package lsp-mode
+;;   :ensure t
+;;   :commands lsp
+;;   :custom
+;;   (lsp-auto-guess-root nil)
+;;   (lsp-prefer-flymake nil) ; Use flycheck instead of flymake
+;;   :bind (:map lsp-mode-map ("C-c C-f" . lsp-format-buffer))
+;;   :hook ((python-mode c-mode c++-mode) . lsp))
+
+
+;; (use-package lsp-ui
+;;   :after lsp-mode
+;;   :ensure t
+;;   :diminish
+;;   :commands lsp-ui-mode
+;;   :custom-face
+;;   (lsp-ui-doc-background ((t (:background nil))))
+;;   (lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic)))))
+;;   :bind (:map lsp-ui-mode-map
+;;               ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+;;               ([remap xref-find-references] . lsp-ui-peek-find-references)
+;;               ("C-c u" . lsp-ui-imenu))
+;;   :custom
+;;   (lsp-ui-doc-enable t)
+;;   (lsp-ui-doc-header t)
+;;   (lsp-ui-doc-include-signature t)
+;;   (lsp-ui-doc-position 'top)
+;;   (lsp-ui-doc-border (face-foreground 'default))
+;;   (lsp-ui-sideline-enable nil)
+;;   (lsp-ui-sideline-ignore-duplicate t)
+;;   (lsp-ui-sideline-show-code-actions nil)
+;;   :config
+;;   ;; Use lsp-ui-doc-webkit only in GUI
+;;   (setq lsp-ui-doc-use-webkit t)
+;;   ;; WORKAROUND Hide mode-line of the lsp-ui-imenu buffer
+;;   ;; https://github.com/emacs-lsp/lsp-ui/issues/243
+;;   (defadvice lsp-ui-imenu (after hide-lsp-ui-imenu-mode-line activate)
+;;     (setq mode-line-format nil)))
+;; #+end_src
+
+
 (provide 'gjg-functions)
