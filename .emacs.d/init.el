8a683c8fe71e1a1f9686a1385db95653e8febb27
Gregory Grubbs <gregory@dynapse.com>
2025-10-15 09:08:58 -0600

    remove unused frame config for mac

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,8 +18,6 @@
 (setq file-name-handler-alist nil)
 (setq site-run-file nil)
 (menu-bar-mode -1)
-(unless (and (display-graphic-p) (eq system-type 'darwin))
-  (push '(menu-bar-lines . 0) default-frame-alist))
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
 ;; (defvar better-gc-cons-threshold 335544320 ; 512MB
